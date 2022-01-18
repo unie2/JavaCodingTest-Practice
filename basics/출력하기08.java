55b83756aec59a0ff48270e86ece55459ba91d44
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-01-18 19:04:37 +0900

    [기초-출력] 1008

---
+++ basics/출력하기08.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\u250C\u252C\u2510");
+        System.out.println("\u251C\u253C\u2524");
+        System.out.println("\u2514\u2534\u2518");
+    }
+}
+
+
+/*
+1. 유니코드를 통해 문자를 출력한다.
+*/
