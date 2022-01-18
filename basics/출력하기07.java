0ffd28d0309e65d58ef64b9f119b8ca5befac185
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-01-18 19:00:36 +0900

    [기초-출력] 1007

---
+++ basics/출력하기07.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\"C:\\Download\\hello.cpp\"");   
+    }
+}
+
+
+/*
+1. 큰따옴표 혹은 \ 문자를 출력할 때는 문자 앞에 백슬래시(\)를 포함시켜 주면된다.
+*/
