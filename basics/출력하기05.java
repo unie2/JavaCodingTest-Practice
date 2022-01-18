61e003fd173c95592120931592289943e73c0cdc
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-01-18 18:56:57 +0900

    [기초-출력] 1005

---
+++ basics/출력하기05.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\"Hello World\"");   
+    }
+}
+
+
+/*
+1. 백슬레시 문구를 넣어줌으로써 큰 따옴표가 포함되어 함께 출력될 수 있도록 한다.
+*/
