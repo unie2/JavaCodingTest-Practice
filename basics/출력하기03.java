e617307bd9509d9ad1beca32b68ffb98cd41a006
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-01-18 18:52:15 +0900

    [기초-출력] 1003

---
+++ basics/출력하기03.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello\nWorld");
+    }
+}
+
+
+/*
+1. 문제에서 요구하는 바와 같이 출력하기 위해 '\n' 을 사용해 줄바꿈 처리하여 출력한다.
+*/
