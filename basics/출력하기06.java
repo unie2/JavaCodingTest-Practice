62700358db9f14bf93d6d5739ce4136db0a3149c
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-01-18 18:58:32 +0900

    [기초-출력] 1006

---
+++ basics/출력하기06.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\"!@#$%^&*()\"");   
+    }
+}
+
+
+/*
+1. 백슬레시를 넣어줌으로써 큰따옴표가 포함된 문자열이 출력될 수 있도록 한다.
+*/
