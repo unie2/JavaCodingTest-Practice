e493b99fb9187b781b958be8e050b916d848e721
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-01-19 14:59:55 +0900

    [기초-입출력] 1015

---
+++ CodeUp/basics/실수 입력받아 둘째 자리까지 출력하기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    float value = sc.nextFloat();
+    System.out.printf("%.2f", value);
+  }
+}
+
+
+/*
+1. 실수를 입력받아 소수점 둘째 자리까지 출력한다.
+*/
