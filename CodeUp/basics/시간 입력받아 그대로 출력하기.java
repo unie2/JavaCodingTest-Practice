41ce52df03c2eff5d1a672690535e4c80d77cf50
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-01-19 15:05:49 +0900

    [기초-입출력] 1018

---
+++ CodeUp/basics/시간 입력받아 그대로 출력하기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String value = sc.next();
+    System.out.println(value);
+  }
+}
+
+
+/*
+1. 문자열 형태로 시간을 입력받고 그대로 출력한다.
+*/
