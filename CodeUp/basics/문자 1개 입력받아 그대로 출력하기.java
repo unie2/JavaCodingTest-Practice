5a4a2daf8d38e3656ea592b44d5c9f5820c0e2c6
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-01-19 14:43:07 +0900

    [기초-입출력] 1011

---
+++ CodeUp/basics/문자 1개 입력받아 그대로 출력하기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    char value = sc.next().charAt(0);
+    System.out.println(value);
+  }
+}
+
+
+/*
+1. 문자 1개를 입력받아 그대로 출력한다.
+*/
