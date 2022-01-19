cc68144de31f891633cb36afecf6cd40a14e818f
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-01-19 14:38:05 +0900

    [기초-입출력] 1010

---
+++ CodeUp/basics/정수 1개 입력받아 그대로 출력하기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int value = sc.nextInt();
+    System.out.println(value);
+  }
+}
+
+
+/*
+1. 정수 1개를 입력받아 그대로 출력한다.
+*/
