8560ae2b1ea46da373a8a74d848fc9503b1e5e8c
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-01-24 08:32:38 +0900

    [기초-입출력] 1021

---
+++ CodeUp/basics/단어 1개 입력받아 그대로 출력하기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String value = sc.next();
+    System.out.println(value);
+  }
+}
+
+
+/*
+1. 단순히 문자열 형태로 입력받아 value에 저장한 뒤 출력한다.
+
+*/
