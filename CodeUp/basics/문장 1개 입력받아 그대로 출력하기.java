eacaf0717783963523032f70b56dfc2f3e83522d
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-01-24 08:35:30 +0900

    [기초-입출력] 1022

---
+++ CodeUp/basics/문장 1개 입력받아 그대로 출력하기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String value = sc.nextLine();
+    System.out.println(value);
+  }
+}
+
+
+/*
+1. 공백 문자가 포함되어 있는 문장을 입력받아 그대로 출력한다.
+2. 단어를 입력받을 때는 next()로 입력받았지만, 문장을 입력받을 때는 nextLine()을 작성하여 입력받는다.
+
+*/
