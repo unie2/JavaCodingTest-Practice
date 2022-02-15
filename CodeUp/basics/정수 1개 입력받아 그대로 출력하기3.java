28e6371cf468ce34de45fe9dc3241b9c14b75766
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-02-16 08:50:43 +0900

    [기초-데이터형] 1030

---
+++ CodeUp/basics/정수 1개 입력받아 그대로 출력하기3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    long data = sc.nextLong();
+
+    System.out.println(data);
+  }
+}
+
+/*
+1. int형으로 입력 값을 받으면 범위가 초과되기 때문에 long형으로 데이터를 입력받아 그대로 출력한다.
+
+*/
