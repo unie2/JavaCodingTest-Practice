4a2815f5934fe7b67be73c27d299f4a56050e9ed
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-02-23 08:29:34 +0900

    [기초-출력변환] 1036

---
+++ CodeUp/basics/영문자 1개 입력받아 10진수로 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    char value = sc.nextLine().charAt(0);
+    int result = (int) value;
+
+    System.out.println(result);
+  }
+}
