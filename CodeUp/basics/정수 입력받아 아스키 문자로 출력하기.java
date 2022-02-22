5b71c1a8f2478e51891553fd662699eb25b0be61
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-02-23 08:30:07 +0900

    [기초-출력변환] 1037

---
+++ CodeUp/basics/정수 입력받아 아스키 문자로 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int value = sc.nextInt();
+    System.out.println((char) value);
+  }
+}
+
+
+// 1. 10진 정수 1개를 입력받아 아스키 문자로 변환하여 출력한다.
