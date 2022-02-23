c030fec6a134b93358f47781465f5a33cc3394c0
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-02-24 08:56:26 +0900

    [기초-산술연산] 1040

---
+++ CodeUp/basics/정수 1개 입력받아 부호 바꿔 출력하기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int value = sc.nextInt();
+
+    System.out.println(-value);
+  }
+}
+
+/*
+1. 정수 하나를 입력받아 부호를 바꿔 출력한다.
+
+2. 단항 연산자인 -(negative)를 변수 앞에 붙여줌으로써 부호를 반대로 변환할 수 있다.
+
+*/
