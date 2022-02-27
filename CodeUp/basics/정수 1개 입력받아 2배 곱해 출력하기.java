babe891ae6d0c82cf45d8460f756e843f6b518ce
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-02-28 08:47:35 +0900

    [기초-비트시프트연산] 1047

---
+++ CodeUp/basics/정수 1개 입력받아 2배 곱해 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int value = sc.nextInt();
+
+    System.out.println(value << 1);
+  }
+}
+
+
+// 1. 비트단위시프트연산자를 이용해 입력받은 정수 값의 2배 값을 출력한다.
