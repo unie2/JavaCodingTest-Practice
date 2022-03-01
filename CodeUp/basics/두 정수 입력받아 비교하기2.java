1942e7d18a9d6320007f8714dfd816d780815a36
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-03-02 08:41:40 +0900

    [기초-비교연산] 1050

---
+++ CodeUp/basics/두 정수 입력받아 비교하기2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    if (a == b)
+      System.out.println(1);
+    else 
+      System.out.println(0);
+  }
+}
+
+// 1. 두 정수(a, b)를 입력받아 a와 b가 같으면 1을, 같지 않으면 0을 출력한다.
