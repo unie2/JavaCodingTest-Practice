3fa428c37593d6f84702db70d58e892206456b0a
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-03-05 09:05:37 +0900

    [기초-논리] 1057

---
+++ CodeUp/basics/참, 거짓이 서로 같을 때에만 참 출력하기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int first = sc.nextInt();
+    int second = sc.nextInt();
+
+    if (first == second)
+      System.out.println(1);
+    else
+      System.out.println(0);
+  }
+}
+
+// 1. 두 정수값을 입력받아 두 값이 서로 같을 경우 1을 출력하고, 그렇지 않으면 0을 출력한다.
