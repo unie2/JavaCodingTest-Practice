c3361a66441383086f787d0b4b787b217882293e
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-03-11 09:05:22 +0900

    [기초-비트단위논리연산] 1060

---
+++ CodeUp/basics/비트단위로 AND 하여 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int first = sc.nextInt();
+    int second = sc.nextInt();
+
+    System.out.println(first & second);
+  }
+}
+
+// 1. 입력된 정수 두 개를 비트 단위 AND로 연산한 후 그 결과를 정수로 출력한다.
