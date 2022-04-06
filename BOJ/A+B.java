bb447fee6fd5aa02ca07577168c7247291584d4a
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-04-07 08:23:00 +0900

    [수학, 구현, 사칙연산] 1000

---
+++ BOJ/A+B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a + b);
+	}
+}
+
+// 1. 두 정수 a와 b를 입력받은 후 두 값을 더한 결과를 출력한다.
