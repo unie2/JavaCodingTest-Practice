5d5d709565cf1e9039a5493555dad1e40bc1fdb7
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-04-09 14:55:40 +0900

    [수학, 구현, 사칙연산] 11021

---
+++ BOJ/A+B - 7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		for (int i=0; i<t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.printf("Case #%d: %d\n", i+1, a + b);
+		}
+	}
+}
+
+// 1. 두 정수 a와 b를 입력받은 후 문제에서 요구하는 출력 형식에 맞추어 두 수의 합을 출력한다.
