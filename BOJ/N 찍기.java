c04cf41a26d1a78d3360c6933887cda64f3c7add
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-04-11 08:42:47 +0900

    [구현] 2741

---
+++ BOJ/N 찍기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		for (int i=1; i<=n; i++)
+			System.out.println(i);
+	}
+}
+
+// 1. 자연수 n을 입력받아 반복문을 수행하여 1부터 n까지 한 줄에 하나씩 값을 출력한다.
