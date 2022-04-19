4498947cb678866594891b02b3f57dbc29cec721
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-04-20 08:23:19 +0900

    [구현] 2522

---
+++ BOJ/별 찍기 - 12.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		for (int i=1; i<=n; i++) {
+			for (int j=n-i; j>=1; j--) {
+				System.out.print(" ");
+			}
+			for (int j=1; j<=i; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+		
+		for (int i=1; i<=n-1; i++) {
+			for (int j=1; j<=i; j++) {
+				System.out.print(" ");
+			}
+			for (int j=n-i; j>=1; j--) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+}
+
+// 1. 입력받은 수를 바탕으로 반복문을 통해 문제에서 요구하는 출력형식에 맞추어 별을 출력한다.
