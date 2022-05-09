0e116361c91946ff0ce02e2e49324b6b376b4886
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-05-10 08:11:01 +0900

    [다이나믹 프로그래밍] 11722

---
+++ BOJ/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] data = new int[n];
+		String[] str = br.readLine().split(" ");
+		int[] dp = new int[n];
+		
+		for (int i=0; i<n; i++) {
+			data[i] = Integer.parseInt(str[i]);
+			dp[i] = 1;
+		}
+		
+		int result = 0;
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<i; j++) {
+				if (data[j] > data[i]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+			result = Math.max(result, dp[i]);
+		}
+		
+		System.out.println(result);
+	}
+}
+
+/*
+1. 감소하는 수열을 바탕으로 가장 첫번째 요소는 자기 자신 포함하여 1이므로, dp 배열을 1로 초기화 한다.
+2. 이중 for문을 통해 현재 확인하고 있는 값과 그 이전에 있는 값들을 하나씩 확인하여 이전 값이 더 클 경우 dp[i]와 dp[j]+1 값을 비교하여 더 큰 값을 dp[i]에 갱신한다.
+3. 내부 반복문이 종료될 때마다 현재 result 값과 dp[i]의 값을 비교하여 더 큰 값으로 result를 갱신한다.
+4. 반복문이 모두 종료되면 dp 배열에서 가장 큰 값을 출력한다.
+
+*/
