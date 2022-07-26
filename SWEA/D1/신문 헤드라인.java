20a3e554b7962c8afb54b37c4b3035a10c049994
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-07-27 08:15:21 +0900

    [D1] 2047

---
+++ SWEA/D1/신문 헤드라인.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		String data = sc.next();
+		data = data.toUpperCase();
+		
+		System.out.println(data);
+	}
+}
+
+// 1. 문자열을 입력받아 toUpperCase() 를 통해 모두 대문자로 변환하여 출력한다.
