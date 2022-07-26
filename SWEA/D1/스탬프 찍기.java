bd2c36d11360803756b022f79cf89b6d92d17325
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-07-27 08:15:48 +0900

    [D1] 2046

---
+++ SWEA/D1/스탬프 찍기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i=0; i<n; i++) {
+			System.out.print("#");
+		}
+	}
+}
+
+// 1. 수를 입력받고 반복문을 통해 입력받은 수만큼 '#'를 출력한다.
