d32aff7bc4442be27ac91cde2c5cb7a0f55c70bd
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-07-28 08:12:28 +0900

    [D1] 2027

---
+++ SWEA/D1/대각선 출력하기.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) throws Exception {
+		System.out.println("#++++");
+		System.out.println("+#+++");
+		System.out.println("++#++");
+		System.out.println("+++#+");
+		System.out.println("++++#");
+		
+	}
+}
+
+// 1. 문제에서 요구하는 출력 형식에 맞추어 그대로 출력한다.
