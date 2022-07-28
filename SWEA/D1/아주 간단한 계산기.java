136eea2a9132b4872168675532cb1cf83a1f9a7b
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-07-29 08:19:00 +0900

    [D1] 1938

---
+++ SWEA/D1/아주 간단한 계산기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println((int)(a/b));
+	}
+}
+
+// 1. a와 b를 정수형으로 입력받고, 두 수의 합, 차, 곱, 나눈 몫 값을 출력한다.
