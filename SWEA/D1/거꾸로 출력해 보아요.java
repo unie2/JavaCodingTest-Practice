f6d70ff25ef6022e136c4a4cdfac84df64ed64c8
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-07-31 17:56:16 +0900

    [D1] 1545

---
+++ SWEA/D1/거꾸로 출력해 보아요.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i=n; i>=0; i--) {
+			System.out.print(i + " ");
+		}
+	}
+}
+
+// 1. n을 입력받아 반복문을 통해 n부터 0까지 출력한다.
